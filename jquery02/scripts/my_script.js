7c52765d227abcf8640dcdbf3bf1750b100c576b
vosaan <vo.saan@gmail.com>
2015-08-25 12:54:42 +0300

    feature01

--- jquery02/scripts/my_script.js
+++ jquery02/scripts/my_script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-	$(".guess_box").click(function(){
-		
-		//$(".guess_box p").remove();
-		
-		var discount = Math.floor((Math.random() * 5) + 5);
-		var discountMessage = "<p>Подавись! " + discount + "%</p>";
-		//alert(discountMessage);
-		$(this).append(discountMessage);
+	function checkForCode(){
+		var my_num = Math.floor((Math.random() * 5) + 5);
+		var discount = "<p>Ваша скидка " + my_num + "%</p>";
+		$(this).append(discount);
+
 		$(".guess_box").each(function(){
 			$(this).unbind("click");
 		});
-	});
+	}
+
+	$(".guess_box").click(checkForCode);
+	
 });
