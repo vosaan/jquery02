bcaac4b05d6af1f50362f2ece46df69570d86822
vosaan <vo.saan@gmail.com>
2015-08-25 12:20:16 +0300

    1

---
+++ jquery02/scripts/my_script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$(".guess_box").click(function(){
+		
+		//$(".guess_box p").remove();
+		
+		var discount = Math.floor((Math.random() * 5) + 5);
+		var discountMessage = "<p>Ваша скидка составляет " + discount + "%</p>";
+		//alert(discountMessage);
+		$(this).append(discountMessage);
+		$(".guess_box").each(function(){
+			$(this).unbind("click");
+		});
+	});
+});
