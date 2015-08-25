f6cdc868389ce53c7281235e8fe130085bf18751
vosaan <vo.saan@gmail.com>
2015-08-25 16:37:50 +0300

    final

--- jquery02/scripts/my_script.js
+++ jquery02/scripts/my_script.js
@@ -1,14 +1,50 @@
 $(document).ready(function(){
-	function checkForCode(){
-		var my_num = Math.floor((Math.random() * 5) + 5);
-		var discount = "<p>Ваша скидка " + my_num + "%</p>";
-		$(this).append(discount);
 
+	$(".guess_box").click(checkForCode);
+
+	function getRandom(num){
+		return Math.floor(Math.random() * num);
+	}
+	
+	var hideCode = function(){
+		var numRnd = getRandom(4);
+		$(".guess_box").each(function(index, value){
+			if(numRnd == index){
+				$(this).append("<span id = 'has_discount'></span>");
+				console.log(numRnd);
+				return false;
+			}
+		});
+	}
+
+	hideCode();
+
+	function checkForCode(){
+		var discount;
+		if($.contains(this, document.getElementById("has_discount"))){
+			discount = "<p>Код скидки: CODE" + getRandom(100) + " </p>";
+		} else {
+			discount = "<p>Ваша скидка 0%</p>";
+		}
+		$(".tablo").append(discount);
 		$(".guess_box").each(function(){
+			if($.contains(this, document.getElementById("has_discount"))){
+				$(this).classAdd("discount");
+			} else {
+				$(this).classAdd("no_discount");
+			}
 			$(this).unbind("click");
 		});
 	}
 
-	$(".guess_box").click(checkForCode);
-	
+	$(".guess_box").hover(
+		function(){
+			$(this).addClass("my_hover");
+		},
+
+		function(){
+			$(this).removeClass("my_hover")
+		});
+
 });
+
---
+++ jquery02/scripts/test_my_script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#btn1").click(function(){
+		$("#header").addClass("hover");
+		$("#header").removeClass("no_hover");
+	});
+
+	$("#btn2").click(function(){
+		$("#header").removeClass("hover");
+		$("#header").addClass("no_hover");
+	});
+});
